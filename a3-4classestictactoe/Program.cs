a9ed1ba97618624e29f52d9f083f13d93a29a7e3
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-07-27 11:12:56 -0600

    Add project files.

---
+++ a3-4classestictactoe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace a3_4classestictactoe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
