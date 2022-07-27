50438e5fd0110026830e41df55f33640b6142789
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-07-27 12:03:33 -0600

    beginning of program started

--- a3-4classestictactoe/Program.cs
+++ a3-4classestictactoe/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace a3_4classestictactoe
 {
     internal class Program
     {
-        static void Main(string[] args)
+        int p1w = 0; // player one wins if = 1
+        int p2w = 0; // player two wins if = 1
+        
+        //0,0 = 1
+        //1,1 = 5
+        //2,2 = 9
+
+        //1 2 3
+        //4 5 6
+        //7 8 9 will change all numbers to zero when complete
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            List<string> Usabletic = new List<string>();
+            Usabletic.Add("a1");
+            Usabletic.Add("a2");
+            Usabletic.Add("a3");
+            Usabletic.Add("b1");
+            Usabletic.Add("b2");
+            Usabletic.Add("b3");
+            Usabletic.Add("c1");
+            Usabletic.Add("c2");
+            Usabletic.Add("c3");
+
+
+            int[,] Tic = /*new int[,]*/{
+            { 1, 2, 3 }, {4,5,6}, {7,8,9}
+            };
+
+
+
+            int i = 0;
+            while (i < 10)
+            {
+
+                i++;
+            }
         }
     }
 }
