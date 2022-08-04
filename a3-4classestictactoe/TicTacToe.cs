9ec86fc437bc52270f75c9f2b550ee816a400893
4400112995 <4400112995@BT144-PROG12>
2022-08-04 10:56:19 -0600

    creation of tictactoe class

---
+++ a3-4classestictactoe/TicTacToe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a3_4classestictactoe
+{
+    class TicTacToe
+    {
+        
+
+    }
+}
